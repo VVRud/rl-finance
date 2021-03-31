e33894f6e87b8b7eac494df705683a9794aaa642
Vladyslav <vl.val.rud@gmail.com>
2021-03-31 22:06:05 +0300

    Database from worker updated

--- app/api/celery_worker/tasks_full.py
+++ app/api/celery_worker/tasks_full.py
@@ -1,7 +1,7 @@
 import datetime
+from db import pg_db, mongo_db
 from finances import fh
 from celery_worker.worker import celery_app
-from celery_worker.db_tasks import PostgresTask, MongoTask
 
 
 def fill_name_value(results, name, value):
@@ -10,139 +10,139 @@ def fill_name_value(results, name, value):
     return results
 
 
-@celery_app.task(name="stock_candles_full", base=PostgresTask, bind=True, queue="stock_candles")
+@celery_app.task(name="stock_candles_full", queue="stock_candles")
 async def full_retrieve_stock_candles(
-    self, symbol: str, c_id: int, resolution: str,
+    symbol: str, c_id: int, resolution: str,
     startdate: datetime.datetime, enddate: datetime.datetime
 ):
     result = await fh.get_stock_candles(symbol, resolution, startdate, enddate)
     if len(result) != 0:
         result = fill_name_value(result, "c_id", c_id)
         result = fill_name_value(result, "resolution", resolution)
-        await (await self.db).insert_stock_candles(result)
+        await pg_db.insert_stock_candles(result)
 
 
-@celery_app.task(name="balance_sheets_full", base=MongoTask, bind=True)
-async def full_retrieve_balance_sheets(self, symbol: str, c_id: int):
+@celery_app.task(name="balance_sheets_full")
+async def full_retrieve_balance_sheets(symbol: str, c_id: int):
     result = await fh.get_balance_sheets(symbol)
     if len(result) != 0:
-        await (await self.db).insert_balance_sheets(fill_name_value(result, "c_id", c_id))
+        await mongo_db.insert_balance_sheets(fill_name_value(result, "c_id", c_id))
 
 
-@celery_app.task(name="cash_flows_full", base=MongoTask, bind=True)
-async def full_retrieve_cash_flows(self, symbol: str, c_id: int):
+@celery_app.task(name="cash_flows_full")
+async def full_retrieve_cash_flows(symbol: str, c_id: int):
     result = await fh.get_cash_flows(symbol)
     if len(result) != 0:
-        await (await self.db).insert_cash_flows(fill_name_value(result, "c_id", c_id))
+        await mongo_db.insert_cash_flows(fill_name_value(result, "c_id", c_id))
 
 
-@celery_app.task(name="income_statements_full", base=MongoTask, bind=True)
-async def full_retrieve_income_statements(self, symbol: str, c_id: int):
+@celery_app.task(name="income_statements_full")
+async def full_retrieve_income_statements(symbol: str, c_id: int):
     result = await fh.get_income_statements(symbol)
     if len(result) != 0:
-        await (await self.db).insert_income_statements(fill_name_value(result, "c_id", c_id))
+        await mongo_db.insert_income_statements(fill_name_value(result, "c_id", c_id))
 
 
-@celery_app.task(name="similarities_full", base=PostgresTask, bind=True)
-async def full_retrieve_similarities(self, symbol: str, c_id: int):
+@celery_app.task(name="similarities_full")
+async def full_retrieve_similarities(symbol: str, c_id: int):
     result = await fh.get_similarity_index(symbol)
     if len(result) != 0:
-        await (await self.db).insert_sec_similarity(fill_name_value(result, "c_id", c_id))
+        await pg_db.insert_sec_similarity(fill_name_value(result, "c_id", c_id))
 
 
-@celery_app.task(name="sentiments_full", base=PostgresTask, bind=True)
+@celery_app.task(name="sentiments_full")
 async def full_retrieve_sentiments(
-    self, symbol: str, c_id: int,
+    symbol: str, c_id: int,
     startdate: datetime.datetime, enddate: datetime.datetime
 ):
     filings = await fh.get_filings(symbol, startdate, enddate)
     result = await fh.get_sec_sentiments(filings)
     if len(result) != 0:
-        await (await self.db).insert_sec_sentiment(fill_name_value(result, "c_id", c_id))
+        await pg_db.insert_sec_sentiment(fill_name_value(result, "c_id", c_id))
 
 
-@celery_app.task(name="dividents_full", base=PostgresTask, bind=True)
+@celery_app.task(name="dividents_full")
 async def full_retrieve_dividents(
-    self, symbol: str, c_id: int,
+    symbol: str, c_id: int,
     startdate: datetime.datetime, enddate: datetime.datetime
 ):
     result = await fh.get_dividends(symbol, startdate, enddate)
     if len(result) != 0:
-        await (await self.db).insert_dividends(fill_name_value(result, "c_id", c_id))
+        await pg_db.insert_dividends(fill_name_value(result, "c_id", c_id))
 
 
-@celery_app.task(name="press_releases_full", base=MongoTask, bind=True)
+@celery_app.task(name="press_releases_full")
 async def full_retrieve_press_releases(
-    self, symbol: str, c_id: int,
+    symbol: str, c_id: int,
     startdate: datetime.datetime, enddate: datetime.datetime
 ):
     result = await fh.get_press_releases(symbol, startdate, enddate)
     if len(result) != 0:
-        await (await self.db).insert_prs(fill_name_value(result, "c_id", c_id))
+        await mongo_db.insert_prs(fill_name_value(result, "c_id", c_id))
 
 
-@celery_app.task(name="splits_full", base=PostgresTask, bind=True)
-async def full_retrieve_splits(self, symbol: str, c_id: int, startdate: datetime.datetime, enddate: datetime.datetime):
+@celery_app.task(name="splits_full")
+async def full_retrieve_splits(symbol: str, c_id: int, startdate: datetime.datetime, enddate: datetime.datetime):
     result = await fh.get_splits(symbol, startdate, enddate)
     if len(result) != 0:
-        await (await self.db).insert_splits(fill_name_value(result, "c_id", c_id))
+        await pg_db.insert_splits(fill_name_value(result, "c_id", c_id))
 
 
-@celery_app.task(name="trends_full", base=PostgresTask, bind=True)
-async def full_retrieve_trends(self, symbol: str, c_id: int):
+@celery_app.task(name="trends_full")
+async def full_retrieve_trends(symbol: str, c_id: int):
     result = await fh.get_trends(symbol)
     if len(result) != 0:
-        await (await self.db).insert_trends(fill_name_value(result, "c_id", c_id))
+        await pg_db.insert_trends(fill_name_value(result, "c_id", c_id))
 
 
-@celery_app.task(name="eps_surprises_full", base=PostgresTask, bind=True)
-async def full_retrieve_eps_surprises(self, symbol: str, c_id: int):
+@celery_app.task(name="eps_surprises_full")
+async def full_retrieve_eps_surprises(symbol: str, c_id: int):
     result = await fh.get_eps_surprises(symbol)
     if len(result) != 0:
-        await (await self.db).insert_eps_surprises(fill_name_value(result, "c_id", c_id))
+        await pg_db.insert_eps_surprises(fill_name_value(result, "c_id", c_id))
 
 
-@celery_app.task(name="eps_estimates_full", base=PostgresTask, bind=True)
-async def full_retrieve_eps_estimates(self, symbol: str, c_id: int):
+@celery_app.task(name="eps_estimates_full")
+async def full_retrieve_eps_estimates(symbol: str, c_id: int):
     result = await fh.get_eps_estimates(symbol)
     if len(result) != 0:
-        await (await self.db).insert_eps_estimates(fill_name_value(result, "c_id", c_id))
+        await pg_db.insert_eps_estimates(fill_name_value(result, "c_id", c_id))
 
 
-@celery_app.task(name="revenue_estimates_full", base=PostgresTask, bind=True)
-async def full_retrieve_revenue_estimates(self, symbol: str, c_id: int):
+@celery_app.task(name="revenue_estimates_full")
+async def full_retrieve_revenue_estimates(symbol: str, c_id: int):
     result = await fh.get_revenue_estimates(symbol)
     if len(result) != 0:
-        await (await self.db).insert_revenue_estimates(fill_name_value(result, "c_id", c_id))
+        await pg_db.insert_revenue_estimates(fill_name_value(result, "c_id", c_id))
 
 
-@celery_app.task(name="upgrades_downgrades_full", base=PostgresTask, bind=True)
+@celery_app.task(name="upgrades_downgrades_full")
 async def full_retrieve_upgrades_downgrades(
-    self, symbol: str, c_id: int,
+    symbol: str, c_id: int,
     startdate: datetime.datetime, enddate: datetime.datetime
 ):
     result = await fh.get_upgrades_downgrades(symbol, startdate, enddate)
     if len(result) != 0:
-        await (await self.db).insert_upgrades_downgrades(fill_name_value(result, "c_id", c_id))
+        await pg_db.insert_upgrades_downgrades(fill_name_value(result, "c_id", c_id))
 
 
-@celery_app.task(name="earnings_calendar_full", base=PostgresTask, bind=True)
+@celery_app.task(name="earnings_calendar_full")
 async def full_retrieve_earnings_calendars(
-    self, symbol: str, c_id: int,
+    symbol: str, c_id: int,
     startdate: datetime.datetime, enddate: datetime.datetime
 ):
     result = await fh.get_earnings_calendars(symbol, startdate, enddate)
     if len(result) != 0:
-        await (await self.db).insert_earnings_calendars(fill_name_value(result, "c_id", c_id))
+        await pg_db.insert_earnings_calendars(fill_name_value(result, "c_id", c_id))
 
 
-@celery_app.task(name="crypto_candles_full", base=PostgresTask, bind=True, queue="crypto_candles")
+@celery_app.task(name="crypto_candles_full", queue="crypto_candles")
 async def full_retrieve_crypto_candles(
-    self, symbol: str, c_id: int, resolution: str,
+    symbol: str, c_id: int, resolution: str,
     startdate: datetime.datetime, enddate: datetime.datetime
 ):
     result = await fh.get_crypto_candles(symbol, resolution, startdate, enddate)
     if len(result) != 0:
         result = fill_name_value(result, "c_id", c_id)
         result = fill_name_value(result, "resolution", resolution)
-        await (await self.db).insert_crypto_candles(result)
+        await pg_db.insert_crypto_candles(result)
--- app/api/celery_worker/worker.py
+++ app/api/celery_worker/worker.py
@@ -1,6 +1,8 @@
 import os
+from db import pg_db, mongo_db
 from celery import Celery
 from celery_worker.celeryconfig import CeleryConfig
+from celery.signals import worker_process_init, worker_process_shutdown
 
 if os.getenv("WORKER_IS_CLIENT", 0) == 1 or os.getenv("WORKER_IS_BEAT", 0) == 1:
     import celery_pool_asyncio # noqa
@@ -11,3 +13,15 @@ celery_app.config_from_object(
     CeleryConfig(),
     force=True
 )
+
+
+@worker_process_init.connect
+async def init_worker(**kwargs):
+    await pg_db.connect()
+
+
+@worker_process_shutdown.connect
+async def shutdown_worker(**kwargs):
+    if pg_db.is_connected:
+        await pg_db.disconnect()
+    mongo_db.close()
