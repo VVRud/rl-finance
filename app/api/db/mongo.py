9788b137848b1225874ff8a14aac50ab8d64eaed
Vladyslav <vl.val.rud@gmail.com>
2021-03-31 20:42:57 +0300

    More fixes to mongo

--- app/api/db/mongo.py
+++ app/api/db/mongo.py
@@ -58,44 +58,51 @@ class MongoCrud(AsyncIOMotorClient):
     async def insert_prs(self, docs: List[dict]):
         async with await self.start_session() as s:
             result = await self.prs_collection.insert_many(docs, session=s)
-        print(result.inserted_ids)
-        return result.inserted_ids
+        if not isinstance(result, list):
+            result = [result]
+        return [str(res) for res in result.inserted_ids]
 
     async def insert_cash_flows(self, docs: List[dict]):
         async with await self.start_session() as s:
             result = await self.cf_collection.insert_many(docs, session=s)
-        print(result.inserted_ids)
-        return result.inserted_ids
+        if not isinstance(result, list):
+            result = [result]
+        return [str(res) for res in result.inserted_ids]
 
     async def insert_income_statements(self, docs: List[dict]):
         async with await self.start_session() as s:
             result = await self.is_collection.insert_many(docs, session=s)
-        print(result.inserted_ids)
-        return result.inserted_ids
+        if not isinstance(result, list):
+            result = [result]
+        return [str(res) for res in result.inserted_ids]
 
     async def insert_balance_sheets(self, docs: List[dict]):
         async with await self.start_session() as s:
             result = await self.bs_collection.insert_many(docs, session=s)
-        print(result.inserted_ids)
-        return result.inserted_ids
+        if not isinstance(result, list):
+            result = [result]
+        return [str(res) for res in result.inserted_ids]
 
     async def insert_insights(self, docs: List[dict]):
         async with await self.start_session() as s:
             result = await self.insights_collection.insert_many(docs, session=s)
-        print(result.inserted_ids)
-        return result.inserted_ids
+        if not isinstance(result, list):
+            result = [result]
+        return [str(res) for res in result.inserted_ids]
 
     async def insert_briefs(self, docs: List[dict]):
         async with await self.start_session() as s:
             result = await self.briefs_collection.insert_many(docs, session=s)
-        print(result.inserted_ids)
-        return result.inserted_ids
+        if not isinstance(result, list):
+            result = [result]
+        return [str(res) for res in result.inserted_ids]
 
     async def insert_other(self, docs: List[dict]):
         async with await self.start_session() as s:
             result = await self.other_collection.insert_many(docs, session=s)
-        print(result.inserted_ids)
-        return result.inserted_ids
+        if not isinstance(result, list):
+            result = [result]
+        return [str(res) for res in result.inserted_ids]
 
     # RETRIEVE
     async def get_prs(self, symbol: str, limit: int = 10, offset: int = 0):
